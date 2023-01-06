d16343ff417a640c33f82e86441b2e8e3f7a13cf
Dominic Michaud <dominicmichaud@Dominics-MacBook-Pro-2.local>
2023-01-06 14:48:13 -0500

    setup initial routing

---
+++ src/routes/games/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Games</h1>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi sint numquam quas magni vero suscipit laboriosam repudiandae fugiat corporis molestiae, odio cum nam neque praesentium! Nulla esse soluta repellendus, corrupti earum aliquam explicabo ea fugit quod commodi omnis sed amet aliquid delectus ducimus maiores dolore recusandae vitae a repellat. Praesentium, debitis voluptatibus sint illum corporis ipsam aliquam maxime voluptates tempora.</p>
+
+<ul>
+    <li><a href="/games/mario-bros">Game 1</a></li>
+    <li><a href="/games/zelda">Game 2</a></li>
+</ul>
