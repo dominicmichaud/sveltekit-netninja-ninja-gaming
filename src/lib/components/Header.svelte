f0948ade5589907d1ec515fa637247682f7a60e4
Dominic Michaud <dominicmichaud@Dominics-MacBook-Pro-2.local>
2023-01-06 17:36:27 -0500

    setup and add components / setup SCSS structure and basic overrides

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,1 +1,15 @@
-<h1>Header</h1>
+<header class="container-fluid">
+    <nav>
+        <ul>
+            <li>
+                <a href="/">Brand</a>
+                <!-- <img src="/controller-logo.svg" alt="Site logo"> -->
+            </li>
+        </ul>
+        <ul>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#" role="button">Button</a></li>
+        </ul>
+    </nav>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<ul>
-    <li><a href="/games">Games</a></li>
-    <li><a href="/about">About</a></li>
-</ul>
+<div class="container-fluid">
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    
+    <ul>
+        <li><a href="/games">Games</a></li>
+        <li><a href="/about">About</a></li>
+    </ul>
+</div>
